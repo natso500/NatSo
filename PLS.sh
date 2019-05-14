6c4f9f239a5c098572e8f4dc9f7d8957463df333
natso500 <49106380+natso500@users.noreply.github.com>
2019-05-14 18:02:05 +0300

    Add files via upload

---
+++ PLS.sh
@@ -0,0 +1,22 @@
+#########sajed#######
+R='\e[99;31m'
+G='\e[99;32m'
+B='\e[99;35m'
+W='\e[99;33m'
+echo
+echo
+echo
+echo -e $G
+echo    "       ++++++++++++++ "
+echo    "    +++++O++++++++O++++++"
+echo    "  ++++++++++++++++++++++++++"
+echo -e $B "  ++++++++(    )++++++++"
+echo    "       ++++++++++++++   "
+echo  -e $W  "                    creat py NatSo "
+echo
+echo -e $R
+
+
+read -p "enter  url  :  " sajed
+cd NatSo11
+python go.py  $sajed
